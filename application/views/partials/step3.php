5c9f04e4d4c1090456e943242656f1ab0f778f7e
Damir Dimitrovici <Damir.Dimitrovici@sensis.com.au>
2016-08-16 09:26:52 +1000

    testing accordion with dropdowns

--- application/views/partials/step3.php
+++ application/views/partials/step3.php
@@ -1,4 +1,4 @@
-<h3>Add opening hours</h3>
+<h3>What times are you open?</h3>
 
 <hr>
 
@@ -11,388 +11,68 @@
 <hr>
 
 <form class="step3">
-	<label><strong>Suggestions</strong>
-		<div class="expanded vertical button-group">
-		  <a class="secondary button hours-btn" data-start="540" data-end="1020">Open Monday to Friday 9:00 - 17:00</a>
-		  <a class="secondary button hours-btn" data-start="0" data-end="1440">Open 24/7</a>
-		  <a class="secondary button no-hours-btn">I don't want to show opening hours</a>
-		</div>
-	</label>
-	
-	<div class="hide-me">
-  <label><strong>Monday</strong>
-   	     <span class="display-times"><span class="slider-time">9:00 AM</span> - <span class="slider-time2">5:00 PM</span></span>
-	  <br>
-	  
-	 <ul class="times">
-		<li>00:00</li>
-		<li>03:00</li>
-		<li>06:00</li>
-		<li>09:00</li>
-		<li>12:00</li>
-		<li>15:00</li>
-		<li>17:00</li>
-		<li>21:00</li>
-		<li>00:00</li>
-	 </ul>
-	  	  
-	<!-- <div class="slider" data-slider data-initial-start='25' data-initial-end='75'>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <span class="slider-fill" data-slider-fill></span>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <input type="hidden">
-	  <input type="hidden">
-	</div> -->
-		
-   	 <div class="time-range">
-   	     <div class="sliders_step1">
-   	         <div class="slider-range"></div>
-   	     </div>
-   	 </div>
-	
-	<ul class="times-detail">
-		<li class="closed-open" data-start="0" data-end="0">Closed</li>
-		<li class="closed-open" data-start="0" data-end="1440">Open 24/7</li>
-		<li class="repeat">Repeat all week</li>
+	<ul class="accordion" data-accordion role="tablist">
+	  <li class="accordion-item">
+	    <!-- The tab title needs role="tab", an href, a unique ID, and aria-controls. -->
+	    <a href="#panel1d" role="tab" class="accordion-title" id="panel1d-heading" aria-controls="panel1d">Monday</a>
+	    <!-- The content pane needs an ID that matches the above href, role="tabpanel", data-tab-content, and aria-labelledby. -->
+	    <div id="panel1d" class="accordion-content" role="tabpanel" data-tab-content aria-labelledby="panel1d-heading">
+	      
+		  <select>
+			<option>1</option>
+			<option>2</option>
+			<option>3</option>
+			<option>4</option>
+			<option>5</option>
+			<option>6</option>
+			<option>7</option>
+			<option>8</option>
+			<option>9</option>
+			<option>10</option>
+			<option>11</option>
+			<option>12</option>
+		  </select>
+		  
+		  <select>
+			<option>00</option>
+			<option>15</option>
+			<option>30</option>
+			<option>45</option>		
+		  </select>
+		  
+		  <select>
+			<option>AM</option>
+			<option>PM</option>
+		  </select>
+			
+	    </div>
+	  </li>
+	  <li class="accordion-item">
+	    <!-- The tab title needs role="tab", an href, a unique ID, and aria-controls. -->
+	    <a href="#panel1d" role="tab" class="accordion-title" id="panel1d-heading" aria-controls="panel1d">Tuesday</a>
+	    <!-- The content pane needs an ID that matches the above href, role="tabpanel", data-tab-content, and aria-labelledby. -->
+	    <div id="panel1d" class="accordion-content" role="tabpanel" data-tab-content aria-labelledby="panel1d-heading">
+	      Panel 2. Lorem ipsum dolor
+	    </div>
+	  </li>
+	  <li class="accordion-item">
+	    <!-- The tab title needs role="tab", an href, a unique ID, and aria-controls. -->
+	    <a href="#panel1d" role="tab" class="accordion-title" id="panel1d-heading" aria-controls="panel1d">Wednesday</a>
+	    <!-- The content pane needs an ID that matches the above href, role="tabpanel", data-tab-content, and aria-labelledby. -->
+	    <div id="panel1d" class="accordion-content" role="tabpanel" data-tab-content aria-labelledby="panel1d-heading">
+	      Panel 3. Lorem ipsum dolor
+	    </div>
+	  </li>
 	</ul>
-  </label>
-  
-  <label><strong>Tuesday</strong>
-   	     <span class="display-times"><span class="slider-time">9:00 AM</span> - <span class="slider-time2">5:00 PM</span></span>
-	  <br>
-	  
-	 <ul class="times">
-		<li>00:00</li>
-		<li>03:00</li>
-		<li>06:00</li>
-		<li>09:00</li>
-		<li>12:00</li>
-		<li>15:00</li>
-		<li>17:00</li>
-		<li>21:00</li>
-		<li>00:00</li>
-	 </ul>
-	  	  
-	<!-- <div class="slider" data-slider data-initial-start='25' data-initial-end='75'>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <span class="slider-fill" data-slider-fill></span>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <input type="hidden">
-	  <input type="hidden">
-	</div> -->
-		
-   	 <div class="time-range">
-   	     <div class="sliders_step1">
-   	         <div class="slider-range"></div>
-   	     </div>
-   	 </div>
 	
-	<ul class="times-detail">
-		<li class="closed-open" data-start="0" data-end="0">Closed</li>
-		<li class="closed-open" data-start="0" data-end="1440">Open 24/7</li>
-		<li class="repeat">Repeat all week</li>
-	</ul>
-  </label>
-  
-  <label><strong>Wednesday</strong>
-   	     <span class="display-times"><span class="slider-time">9:00 AM</span> - <span class="slider-time2">5:00 PM</span></span>
-	  <br>
-	  
-	 <ul class="times">
-		<li>00:00</li>
-		<li>03:00</li>
-		<li>06:00</li>
-		<li>09:00</li>
-		<li>12:00</li>
-		<li>15:00</li>
-		<li>17:00</li>
-		<li>21:00</li>
-		<li>00:00</li>
-	 </ul>
-	  	  
-	<!-- <div class="slider" data-slider data-initial-start='25' data-initial-end='75'>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <span class="slider-fill" data-slider-fill></span>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <input type="hidden">
-	  <input type="hidden">
-	</div> -->
-		
-   	 <div class="time-range">
-   	     <div class="sliders_step1">
-   	         <div class="slider-range"></div>
-   	     </div>
-   	 </div>
-	
-	<ul class="times-detail">
-		<li class="closed-open" data-start="0" data-end="0">Closed</li>
-		<li class="closed-open" data-start="0" data-end="1440">Open 24/7</li>
-		<li class="repeat">Repeat all week</li>
-	</ul>
-  </label>
-  
-  <label><strong>Thursday</strong>
-   	     <span class="display-times"><span class="slider-time">9:00 AM</span> - <span class="slider-time2">5:00 PM</span></span>
-	  <br>
-	  
-	 <ul class="times">
-		<li>00:00</li>
-		<li>03:00</li>
-		<li>06:00</li>
-		<li>09:00</li>
-		<li>12:00</li>
-		<li>15:00</li>
-		<li>17:00</li>
-		<li>21:00</li>
-		<li>00:00</li>
-	 </ul>
-	  	  
-	<!-- <div class="slider" data-slider data-initial-start='25' data-initial-end='75'>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <span class="slider-fill" data-slider-fill></span>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <input type="hidden">
-	  <input type="hidden">
-	</div> -->
-		
-   	 <div class="time-range">
-   	     <div class="sliders_step1">
-   	         <div class="slider-range"></div>
-   	     </div>
-   	 </div>
-	
-	<ul class="times-detail">
-		<li class="closed-open" data-start="0" data-end="0">Closed</li>
-		<li class="closed-open" data-start="0" data-end="1440">Open 24/7</li>
-		<li class="repeat">Repeat all week</li>
-	</ul>
-  </label>
-  
-  <label><strong>Friday</strong>
-   	     <span class="display-times"><span class="slider-time">9:00 AM</span> - <span class="slider-time2">5:00 PM</span></span>
-	  <br>
-	  
-	 <ul class="times">
-		<li>00:00</li>
-		<li>03:00</li>
-		<li>06:00</li>
-		<li>09:00</li>
-		<li>12:00</li>
-		<li>15:00</li>
-		<li>17:00</li>
-		<li>21:00</li>
-		<li>00:00</li>
-	 </ul>
-	  	  
-	<!-- <div class="slider" data-slider data-initial-start='25' data-initial-end='75'>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <span class="slider-fill" data-slider-fill></span>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <input type="hidden">
-	  <input type="hidden">
-	</div> -->
-		
-   	 <div class="time-range">
-   	     <div class="sliders_step1">
-   	         <div class="slider-range"></div>
-   	     </div>
-   	 </div>
-	
-	<ul class="times-detail">
-		<li class="closed-open" data-start="0" data-end="0">Closed</li>
-		<li class="closed-open" data-start="0" data-end="1440">Open 24/7</li>
-		<li class="repeat">Repeat all week</li>
-	</ul>
-  </label>
-  
-  <label><strong>Saturday</strong>
-   	     <span class="display-times"><span class="slider-time">9:00 AM</span> - <span class="slider-time2">5:00 PM</span></span>
-	  <br>
-	  
-	 <ul class="times">
-		<li>00:00</li>
-		<li>03:00</li>
-		<li>06:00</li>
-		<li>09:00</li>
-		<li>12:00</li>
-		<li>15:00</li>
-		<li>17:00</li>
-		<li>21:00</li>
-		<li>00:00</li>
-	 </ul>
-	  	  
-	<!-- <div class="slider" data-slider data-initial-start='25' data-initial-end='75'>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <span class="slider-fill" data-slider-fill></span>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <input type="hidden">
-	  <input type="hidden">
-	</div> -->
-		
-   	 <div class="time-range">
-   	     <div class="sliders_step1">
-   	         <div class="slider-range"></div>
-   	     </div>
-   	 </div>
-	
-	<ul class="times-detail">
-		<li class="closed-open" data-start="0" data-end="0">Closed</li>
-		<li class="closed-open" data-start="0" data-end="1440">Open 24/7</li>
-		<li class="repeat">Repeat all week</li>
-	</ul>
-  </label>
-  
-  <label><strong>Sunday</strong>
-   	     <span class="display-times"><span class="slider-time">9:00 AM</span> - <span class="slider-time2">5:00 PM</span></span>
-	  <br>
-	  
-	 <ul class="times">
-		<li>00:00</li>
-		<li>03:00</li>
-		<li>06:00</li>
-		<li>09:00</li>
-		<li>12:00</li>
-		<li>15:00</li>
-		<li>17:00</li>
-		<li>21:00</li>
-		<li>00:00</li>
-	 </ul>
-	  	  
-	<!-- <div class="slider" data-slider data-initial-start='25' data-initial-end='75'>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <span class="slider-fill" data-slider-fill></span>
-	  <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
-	  <input type="hidden">
-	  <input type="hidden">
-	</div> -->
-		
-   	 <div class="time-range">
-   	     <div class="sliders_step1">
-   	         <div class="slider-range"></div>
-   	     </div>
-   	 </div>
-	
-	<ul class="times-detail">
-		<li class="closed-open" data-start="0" data-end="0">Closed</li>
-		<li class="closed-open" data-start="0" data-end="1440">Open 24/7</li>
-		<li class="repeat">Repeat all week</li>
-	</ul>
-  </label>
-    </div> <!-- /.hide-me -->
-	
-  <div class="input-group">
-	<!-- <a class="expanded button alert" href="#">Skip</a> -->
-	<a class="expanded button" href="#step/4">Next</a>
-  </div>
+  	<div class="input-group">
+		<!-- <a class="expanded button alert" href="#">Skip</a> -->
+		<a class="expanded button" href="#step/4">Next</a>
+	</div>
 </form>
 
 <script>
-	$(function() {
-		$('.no-hours-btn').click(function(){
-			$('.hide-me').slideToggle("slow");
-		});
-		
-		$('.hours-btn').click(function(){
-			$('.hide-me').show("slow");
-			
-			var start = $(this).attr('data-start');
-			var end = $(this).attr('data-end');
-			$(".slider-range").slider({values: [start, end]});
-			
-			switch (end) {
-				case '1020':
-					$(".slider-time").html("9:00 AM");
-					$(".slider-time2").html("5:00 PM");
-					break;
-				case '1440':
-					$(".slider-time").html("12:00 AM");
-					$(".slider-time2").html("11:59 PM");
-					break;
-				default:
-			}
-			
-		});
-		
-		
-		$( ".closed-open" ).click(function() {
-			var start = $(this).attr('data-start');
-			var end = $(this).attr('data-end');
-			
-		  $(this).parent().siblings('.time-range').find('.slider-range').slider({values:[start,end]});
-		  
-		  switch (end) {
-		  case '1440':
-			  $(this).parent().siblings('.display-times').find('.slider-time').html("12:00 AM");
-			  $(this).parent().siblings('.display-times').find('.slider-time2').html("11:59 PM");
-		  	break;
-		  default:
-			  $(this).parent().siblings('.display-times').find('.slider-time').html("0:00 AM");
-			  $(this).parent().siblings('.display-times').find('.slider-time2').html("0:00 AM");
-		  }
-		  
-		});
-		
-		
-		$(".slider-range").slider({
-		    range: true,
-		    min: 0,
-		    max: 1440,
-		    step: 15,
-		    values: [540, 1020],
-		    slide: function (e, ui) {				
-				console.log(ui.values[1]);
-				
-		        var hours1 = Math.floor(ui.values[0] / 60);
-		        var minutes1 = ui.values[0] - (hours1 * 60);
-
-		        if (hours1.length == 1) hours1 = '0' + hours1;
-		        if (minutes1.length == 1) minutes1 = '0' + minutes1;
-		        if (minutes1 == 0) minutes1 = '00';
-		        if (hours1 >= 12) {
-		            if (hours1 == 12) {
-		                hours1 = hours1;
-		                minutes1 = minutes1 + " PM";
-		            } else {
-		                hours1 = hours1 - 12;
-		                minutes1 = minutes1 + " PM";
-		            }
-		        } else {
-		            hours1 = hours1;
-		            minutes1 = minutes1 + " AM";
-		        }
-		        if (hours1 == 0) {
-		            hours1 = 12;
-		            minutes1 = minutes1;
-		        }
-
-
-
-		        $(this).parent().parent().parent().find('.display-times .slider-time').html(hours1 + ':' + minutes1);
-				
-		        var hours2 = Math.floor(ui.values[1] / 60);
-		        var minutes2 = ui.values[1] - (hours2 * 60);
-
-		        if (hours2.length == 1) hours2 = '0' + hours2;
-		        if (minutes2.length == 1) minutes2 = '0' + minutes2;
-		        if (minutes2 == 0) minutes2 = '00';
-		        if (hours2 >= 12) {
-		            if (hours2 == 12) {
-		                hours2 = hours2;
-		                minutes2 = minutes2 + " PM";
-		            } else if (hours2 == 24) {
-		                hours2 = 11;
-		                minutes2 = "59 PM";
-		            } else {
-		                hours2 = hours2 - 12;
-		                minutes2 = minutes2 + " PM";
-		            }
-		        } else {
-		            hours2 = hours2;
-		            minutes2 = minutes2 + " AM";
-		        }
-				
-		        $(this).parent().parent().parent().find('.display-times .slider-time2').html(hours2 + ':' + minutes2);
-		    }
-		});
+	$(function(){
+		// $('.accordion .accordion-item').toggleClass('is-active');
 	});
 </script>
