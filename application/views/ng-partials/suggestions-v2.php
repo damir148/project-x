36a3642b5e4ecb9d3c22a5d63b1b62e0e9c6179c
Doan, Thanh <Thanh.Doan@sensis.com.au>
2016-08-16 13:31:32 +1000

    apply tweaks to suggestions

--- application/views/ng-partials/suggestions-v2.php
+++ application/views/ng-partials/suggestions-v2.php
@@ -1,34 +1,102 @@
-<h3>Here's what we found!</h3>
-<p>We think these are the solutions that best meet your business' needs.</p>
+<div class="suggestions-v2">
+	<div class="callout suggestions-hero row">
+		<div class="small-8 columns">
+			<h3>Here's what we found!</h3>
+			<p>We think these solutions best meet your business' needs.</p>
+		</div>
+		<div class="small-4 columns">
+			HOLA
+		</div>
+	</div>
 
-<hr />
+	<hr />
 
-<h4>Select your preferred option</h4>
+	<div class="free-listing-section">
+		<div class="callout row">
+			<div class="small-8 columns">
+				<h5 class="product-title">Free Listing</h5>
+				<p><strong>$0 per month</strong></p>
+				<p>Be found for free with your basic listing.</p>
+			</div>
+			<div class="small-4 columns">	
+				<a href="#/step/1" class="callout-clickable">Select product now &gt;</a>
+			</div>
+		</div>
+		<div class="columns about" ng-show="showFree">
+			<h6 class="product-about">About Free listings</h6>
+			<p>Business name, address and phone number will appear in the Yellow Pages and White Pages, both in the book and online.</p>
+			<p>Your email address and website will also appear on Yellow Pages online.</p>
+		</div>
+		<div class="column">
+			<button ng-hide="showFree" ng-click="showFree=true">Learn more ^</button>
+			<button ng-show="showFree" ng-click="showFree=false">Show less ^</button>
+		</div>
+	</div>
 
+	<hr />
 
-<div class="callout">
-	<a href="#/product/free" class="callout-clickable">
-		<h5 class="product-title">Free</h5>
-		<p>Be found for free with your basic listing.</p>
-		<span class="fake-click">Show more</span>
-	</a>
-</div>
+	<div class="get-found-listing-section">
+		<div class="callout product-get-found row">
+			<div class="small-8 columns">
+				<h5 class="product-title">Get Found</h5>
+				<p><strong>$51 per month</strong></p>
+				<p>Get found where your customers are looking.</p>
+			</div>
+			<div class="small-4 columns">
+				<a href="#/step/1" class="callout-clickable">Select product now &gt;</a>
+			</div>
+		</div>
+		<div class="columns about" ng-show="showFound">
+			<h6 class="product-about">About Get Found</h6>
+			<ul>
+				<li>Get found where your customers are looking</li>
+				<li>Strong presence in print and digital</li>
+				<li>Track your ROI across all media</li>
+				<li>Drive more leads to your business</li>
+			</ul>
+		</div>
+		<div class="column">
+			<button ng-hide="showFound" ng-click="showFound=true">Learn more ^</button>
+			<button ng-show="showFound" ng-click="showFound=false">Show less ^</button>
+		</div>
+	</div>
 
+	<hr />
 
-<div class="callout product-get-found">
-	<a href="#/product/get-found" class="callout-clickable">
-		<h5 class="product-title">Get Found <span>- $51 per month</span></h5>
-		<p>Get found where your customers are looking</p>
-		<span class="fake-click">Show more</span>
-	</a>
-</div>
+	<div class="extend-reach-listing-section">
+		<div class="callout product-extend-reach row">
+			<div class="small-8 columns">
+				<h5 class="product-title">Extend Reach</h5>
+				<p><strong>$175 per month</strong></p>
+				<p>Put your business in front of a wider audience.</p>
+			</div>
+			<div class="small-4 columns">
+				<a href="#/step/1" class="callout-clickable">Select product now &gt;</a>
+			</div>
+		</div>
+		<div class="columns about" ng-show="showReach">
+			<h6 class="product-about">About extend reach</h6>
+			<ul>
+				<li>Put your business in front of a wider audience</li>
+				<li>Attract customers outside your regular areas</li>
+				<li>Enjoy a prominent listing across our digital network</li>
+				<li>Get a free call tracked phone number to measure your advertising performance</li>
+			</ul>
+		</div>
+		<div class="column">
+			<button ng-hide="showReach" ng-click="showReach=true">Learn more ^</button>
+			<button ng-show="showReach" ng-click="showReach=false">Show less ^</button>
+		</div>
+	</div>
 
+	<hr />
 
-<div class="callout product-extend-reach">
-	<a href="#/product/extend-reach" class="callout-clickable">
-		<h5 class="product-title">Extend Reach <span>- $175 per month</span></h5>
-		<p>Put your business in front of a wider audience.</p>
-		<span class="fake-click">Show more</span>
-	</a>
+	<div class="extra-info-stuff">
+		<h3>After something a little different?</h3>
+		<p>Perhaps try searching with different options selected or maybe we should have a chat.</p>
+	</div>
+
+	<a class="expanded button" href="#/step/1">Search again</a>
+	<a class="expanded button no-thanks-btn" href="#/suggestions">Speak to a specialist</a>
 </div>
 	
