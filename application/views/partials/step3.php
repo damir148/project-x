554d85479987d23a49bb2f73a2b290655dd2e2ec
Damir Dimitrovici <Damir.Dimitrovici@sensis.com.au>
2016-08-16 10:24:20 +1000

    test dropdown ui

--- application/views/partials/step3.php
+++ application/views/partials/step3.php
@@ -52,7 +52,7 @@
 		  
 		  <fieldset>
 			  <legend>To</legend>
-			  <select>
+			  <!-- <select>
 				<option>-</option>
 				<option>1</option>
 				<option>2</option>
@@ -66,15 +66,67 @@
 				<option>10</option>
 				<option>11</option>
 				<option>12</option>
-			  </select>
-		  
+			  </select> -->
+				  
 			  <select>
+				<option>-</option>
+				<option>1:00</option>
+				<option>1:15</option>
+				<option>1:30</option>
+				<option>1:45</option>
+				<option>2:00</option>
+				<option>2:15</option>
+				<option>2:30</option>
+				<option>2:45</option>
+				<option>3:00</option>
+				<option>3:15</option>
+				<option>3:30</option>
+				<option>3:45</option>
+				<option>4:00</option>
+				<option>4:15</option>
+				<option>4:30</option>
+				<option>4:45</option>
+				<option>5:00</option>
+				<option>5:15</option>
+				<option>5:30</option>
+				<option>5:45</option>
+				<option>6:00</option>
+				<option>6:15</option>
+				<option>6:30</option>
+				<option>6:45</option>
+				<option>7:00</option>
+				<option>7:15</option>
+				<option>7:30</option>
+				<option>7:45</option>
+				<option>8:00</option>
+				<option>8:15</option>
+				<option>8:30</option>
+				<option>8:45</option>
+				<option>9:00</option>
+				<option>9:15</option>
+				<option>9:30</option>
+				<option>9:45</option>
+				<option>10:00</option>
+				<option>10:15</option>
+				<option>10:30</option>
+				<option>10:45</option>
+				<option>11:00</option>
+				<option>11:15</option>
+				<option>11:30</option>
+				<option>11:45</option>
+				<option>12:00</option>
+				<option>12:15</option>
+				<option>12:30</option>
+				<option>12:45</option>
+			  </select>
+		  	  
+			  <!-- <select>
 				<option>-</option>
 				<option selected>00</option>
 				<option>15</option>
 				<option>30</option>
-				<option>45</option>		
-			  </select>
+				<option>45</option>
+			  </select> -->
 		  
 			  <select>
 				<option>-</option>
