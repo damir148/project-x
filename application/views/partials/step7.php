1aeff0b045f877fd92c52e6c9697fdbed1df8ef3
Doan, Thanh <Thanh.Doan@sensis.com.au>
2016-08-17 16:51:05 +1000

    added step7

---
+++ application/views/partials/step7.php
@@ -0,0 +1,19 @@
+<div class="rushed-job-at-its-finest">
+	<img src="/static/img/step7/01.jpg" />
+	<a href="/#/step/8">
+		<img src="/static/img/step7/02.jpg" />
+	</a>
+</div>
+
+<style>
+.rushed-job-at-its-finest {
+	margin-left: -.9375rem;
+	margin-right: -.9375rem;
+}
+
+.rushed-job-at-its-finest img {
+	width: 100%;
+	height: auto;
+}
+</style>
+
