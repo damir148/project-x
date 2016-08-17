68207eff181e5e1b45f8bd5cd10549f1d799104c
Doan, Thanh <Thanh.Doan@sensis.com.au>
2016-08-17 16:42:16 +1000

    fix margins

--- application/views/partials/step6.php
+++ application/views/partials/step6.php
@@ -1,14 +1,17 @@
-<img src="/static/img/step6/01.jpg" />
-<a href="/#/step/7">
-  <img src="/static/img/step6/02.jpg" />
-</a>
-<br />
-<br />
-<img src="/static/img/step6/03.jpg" />
-<img src="/static/img/step6/04.jpg" />
+<div class="rushed-job-at-its-finest">
+	<img src="/static/img/step6/01.jpg" />
+	<a href="/#/step/7">
+		<img src="/static/img/step6/02.jpg" />
+	</a>
+	<br />
+	<br />
+	<img src="/static/img/step6/03.jpg" />
+	<img src="/static/img/step6/04.jpg" />
+</div>
 
 <style>
-.columns {
-  padding: 0;
+.rushed-job-at-its-finest {
+	margin-left: -.9375rem;
+	margin-right: -.9375rem;
 }
 </style>
