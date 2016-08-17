1ad7688864180015fba123c2e8cd6232748a2cd5
Doan, Thanh <Thanh.Doan@sensis.com.au>
2016-08-18 09:53:06 +1000

    .

--- application/views/partials/step7.php
+++ application/views/partials/step7.php
@@ -1,7 +1,43 @@
 <div class="rushed-job-at-its-finest">
 	<img src="/static/img/step7/01.jpg" />
+</div>
+<div class="columns">
+	<div class="ui checkbox">
+		<input type="checkbox" name="example" id="1">
+		<label for="1">Add to cart</label>
+	</div>
+</div>
+<div class="rushed-job-at-its-finest">
+	<img src="/static/img/step7/02.jpg" />
+</div>
+<div class="columns">
+	<div class="ui checkbox">
+		<input type="checkbox" name="example" id="2">
+		<label for="2">Add to cart</label>
+	</div>
+</div>
+<div class="rushed-job-at-its-finest">
+	<img src="/static/img/step7/03.jpg" />
+</div>
+<div class="columns">
+	<div class="ui checkbox">
+		<input type="checkbox" name="example" id="3">
+		<label for="3">Add to cart</label>
+	</div>
+</div>
+<div class="rushed-job-at-its-finest">
+	<img src="/static/img/step7/04.jpg" />
+</div>
+<div class="columns">
+	<div class="ui checkbox">
+		<input type="checkbox" name="example" id="4">
+		<label for="4">Add to cart</label>
+	</div>
+</div>
+<div class="rushed-job-at-its-finest">
+	<img src="/static/img/step7/05.jpg" />
 	<a href="/#/step/8">
-		<img src="/static/img/step7/02.jpg" />
+		<img src="/static/img/step7/06.jpg" />
 	</a>
 </div>
 
@@ -11,9 +47,19 @@
 	margin-right: -.9375rem;
 }
 
+label {
+	font-weight: bold;
+}
+
 .rushed-job-at-its-finest img {
 	width: 100%;
 	height: auto;
 }
-</style>
 
+.ui.checkbox .box:before,
+.ui.checkbox label:before {
+    left: -1px;
+    width: 20px;
+    height: 22px;
+}
+</style>
