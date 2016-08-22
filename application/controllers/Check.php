af644872f053029722d807dc1e1ad3b8cf7bc862
Doan, Thanh <Thanh.Doan@sensis.com.au>
2016-08-22 12:49:45 +1000

    check user agent CI way

--- application/controllers/Check.php
+++ application/controllers/Check.php
@@ -4,11 +4,11 @@ class Check extends CI_Controller {
 
 	public function index()
 	{
-		if ( preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]) ) {
-			redirect('/homepage/index', 'refresh');
+		if ( $this->agent->is_mobile() ) {
+			redirect(base_url().'homepage/index', 'refresh');
 		}
 		else {
-			redirect('http://mysensis-poc.laws-sensis.com.au/emulator/iphone.php?url=mysensis-poc.laws-sensis.com.au', 'refresh');
+			redirect(base_url().'emulator/iphone.php?url=mysensis-poc.laws-sensis.com.au', 'refresh');
 		}
 	}
 
