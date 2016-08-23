5b3431ea8f5298487de56bcbb1971cf3be7e3f41
Doan, Thanh <Thanh.Doan@sensis.com.au>
2016-08-23 15:23:02 +1000

    add dashboard04

---
+++ application/views/partials/dashboard04.php
@@ -0,0 +1,35 @@
+<h3 id="scrollTo">Contact details</h3>
+
+<form>
+	<label>Email address
+		<input type="email" value="info@hotondohomes.com.au">
+	</label>
+	<label>Phone number
+		<input type="text" value="1800 677 156">
+	</label>
+	<label>Facebook URL
+		<input type="url" value="facebook.com/hotondohomes" >
+	</label>
+	<label>Twitter URL
+		<input type="url">
+	</label>
+	<label>Website URL
+		<input type="url" value="hotondohomes.com.au">
+	</label>
+	<label>Postal Address
+		<textarea rows="5">57 Cochranes Rd, Moorabbin VIC 3189</textarea>
+	</label>
+
+	<div class="input-group">
+		<a class="expanded button" href="#/dashboard03">Save</a>
+	</div>
+</form>
+
+<style>
+	a.button,
+	a.button:hover,
+	a.button:focus {
+		border-radius: 15px !important;
+		background-color: #1e7be2;
+	}
+</style>
