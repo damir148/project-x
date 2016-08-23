601b66cefa211a7e2a85230620865ad641c43ead
Doan, Thanh <Thanh.Doan@sensis.com.au>
2016-08-23 14:53:32 +1000

    added dashboard 03

---
+++ application/views/partials/dashboard03.php
@@ -0,0 +1,26 @@
+<div class="rushed-job-at-its-finest">
+	<a href="#/dashboard02">
+		<img src="static/img/dashboard/03/01.jpg" />
+	</a>
+	<img src="static/img/dashboard/03/02.jpg" />
+	<img src="static/img/dashboard/03/03.jpg" />
+	<img src="static/img/dashboard/03/04.jpg" />
+	<img src="static/img/dashboard/03/05.jpg" />
+	<img src="static/img/dashboard/03/06.jpg" />
+	<img src="static/img/dashboard/03/07.jpg" />
+	<img src="static/img/dashboard/03/08.jpg" />
+	<img src="static/img/dashboard/03/09.jpg" />
+	<img src="static/img/dashboard/03/10.jpg" />
+	<img src="static/img/dashboard/03/11.jpg" />
+	<a href="#/dashboard03">
+		<img src="static/img/dashboard/03/12.jpg" />
+	</a>
+</div>
+
+<style>
+	.rushed-job-at-its-finest {
+		margin-left: -.9375rem;
+		margin-right: -.9375rem;
+		padding-bottom: 5%;
+	}
+</style>
